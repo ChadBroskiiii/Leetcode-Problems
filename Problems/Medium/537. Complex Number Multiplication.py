d6175bbd5e76798dbba7341d0e0394bed4107bd8
Will Felton <williamfelton2006@icloud.com>
2024-04-16 14:11:27 +0100

    Add files via upload

---
+++ Problems/Medium/537. Complex Number Multiplication.py
@@ -0,0 +1,12 @@
+class Solution:
+    def complexNumberMultiply(self, num1: str, num2: str) -> str:
+        real1 = num1.split("+")
+        real2 = num2.split("+")
+        imaginary1 = real1[1].split("i")
+        imaginary2 = real2[1].split("i")
+        realsol = int(real1[0]) * int(real2[0]) - int(imaginary1[0]) * int(imaginary2[0])
+        print(int(imaginary1[0]), int(imaginary2[0]))
+        imaginarysol = (int(real1[0]) * int(imaginary2[0])) + (int(imaginary1[0]) * int(real2[0]))
+        return (str(realsol) + "+" + str(imaginarysol) + "i")
+solinst = Solution()
+print(solinst.complexNumberMultiply(num1="78+-76i", num2="-86+72i"))
