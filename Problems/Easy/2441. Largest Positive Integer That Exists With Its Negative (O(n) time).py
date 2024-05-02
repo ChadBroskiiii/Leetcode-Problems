32c73039ee96e535fd2d19812dc78f2de1212d39
William Felton <williamfelton2006@icloud.com>
2024-05-02 11:07:39 +0100

    2441 changes and new faster one

---
+++ Problems/Easy/2441. Largest Positive Integer That Exists With Its Negative (O(n) time).py
@@ -0,0 +1,16 @@
+class Solution:
+    def findMaxK(self, nums: list[int]) -> int:
+        tempmax = 0
+        for i in range(len(nums)):
+            if abs(nums[i]) > tempmax:
+                if nums[i] < 0:
+                    if abs(nums[i]) in nums:
+                        tempmax = abs(nums[i])
+                elif nums[i] > 0:
+                    if -nums[i] in nums:
+                        tempmax = abs(nums[i])
+        
+        if tempmax == 0:
+            return -1
+        else:
+            return tempmax
